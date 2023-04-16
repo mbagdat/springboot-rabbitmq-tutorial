36d07548e5f631fe01a4eaca60aa5e8c16d26f56
Mutlu Bağdat <36661375+mbagdat@users.noreply.github.com>
2023-04-17 02:02:59 +0300

    time convert and insert enumType

--- src/main/java/net/javaguides/springboot/consumer/RabbitMQJsonConsumer.java
+++ src/main/java/net/javaguides/springboot/consumer/RabbitMQJsonConsumer.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.javaguides.springboot.dto.User;
+import net.javaguides.springboot.enumuration.UserStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,6 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class RabbitMQJsonConsumer {
@@ -23,8 +27,18 @@ public class RabbitMQJsonConsumer {
     private String emailFrom;
 
     @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
-    public void consumeJsonMessage(User user){
+    public void consumeJsonMessage(User user) {
         LOGGER.info(String.format("Received JSON message -> %s", user.toString()));
+
+        user.setDateSent(new Date()); // burada tarih bilgisi atanıyor
+        user.setStatus(UserStatus.PASSIVE); // burada enum değeri atanıyor
+
+
+        long unixTime = user.getDateSent().getTime();
+        Date date = new Date(unixTime);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String formattedDate = formatter.format(date);
+
 
         // User objesini JSON formatına dönüştürme işlemi
         ObjectMapper mapper = new ObjectMapper();
@@ -40,7 +54,10 @@ public class RabbitMQJsonConsumer {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo("bagdatmutlu@gmail.com");
         message.setSubject("Yeni mesaj alındı!");
-        message.setText("Yeni bir JSON mesajı alındı: " + userJson);
+//        message.setText("Yeni bir JSON mesajı alındı: " + userJson);
+//        message.setText("Yeni bir JSON mesajı alındı: " + userJson+ "\n\nGönderilme zamanı: " + formattedDate);
+        message.setText("Yeni bir JSON mesajı alındı: " + userJson.replace("\"dateSent\":" + user.getDateSent().getTime() + ",", "") + "\n\nGönderilme zamanı: " + formattedDate);
+
         message.setReplyTo(emailFrom);
         javaMailSender.send(message);
     }
--- src/main/java/net/javaguides/springboot/dto/User.java
+++ src/main/java/net/javaguides/springboot/dto/User.java
@@ -1,10 +1,26 @@
 package net.javaguides.springboot.dto;
 
 import lombok.Data;
+import net.javaguides.springboot.enumuration.UserStatus;
+
+import java.util.Date;
 
 @Data
 public class User {
     private int id;
     private String firstName;
     private String lastName;
+    private Date dateSent;
+    private UserStatus status;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
